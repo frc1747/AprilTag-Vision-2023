0c5954eec1db364c1b2e60cfca0d55501f060d85
Cobo04 <cschu88@tscstudents.net>
2023-02-11 15:28:58 -0500

    Game Piece Detection
    Added Game Piece Detection with NetworkTables

--- gamePiece.py
+++ gamePiece.py
@@ -6,6 +6,62 @@ import argparse
 import cv2
 import imutils
 import time
+from networktables import NetworkTables
+import threading
+
+cap = cv2.VideoCapture(1)
+
+# Network Tables Initiation
+
+TEAM_IP = 'roborio-1747-frc.local'
+TIMEOUT_TIME = 20
+TABLE_NAME = 'SmartDashboard'
+
+def initNetworkTables():
+    def connectionListener(connected, info):
+        print(info, '; Connected=%s' % connected)
+        with cond:
+            notified[0] = True
+            cond.notify()
+
+    print('Attempting to connect to RoboRio. Timing out in',TIMEOUT_TIME,'seconds.')
+    # Start timeout timer
+    startTime = time.time()
+
+    cond = threading.Condition()
+    notified = [False]
+
+    # As a client to connect to a robot
+    serverIP = TEAM_IP
+    NetworkTables.initialize(server=serverIP)
+    NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
+    
+    with cond:
+        # Check if we connected
+        print("Waiting")
+        if not notified[0]:
+            # Keep waiting until we connect or time out
+            # Returns true if sucess, false if timed out.
+            connectionState = cond.wait(TIMEOUT_TIME)
+    
+    # Check if we timed out.
+    if connectionState == False:
+        print("Connection Timed out after",TIMEOUT_TIME,"seconds.")
+        quit()
+
+    print("Connection Established!")
+    
+    global nTable
+    nTable = NetworkTables.getTable(TABLE_NAME)
+    nTable.putBoolean('connected', True)
+    if nTable.getBoolean('connected',defaultValue=False):
+        print('Connected to RoboRio at', serverIP)
+    else:
+        print('Connection Failed')
+
+initNetworkTables()
+
+
 
 ap = argparse.ArgumentParser()
 
@@ -13,7 +69,7 @@ args = vars(ap.parse_args())
 
 # Camera Resolution: 640 x 480
 
-vs = VideoStream(src=2).start()
+vs = VideoStream(src=1).start()
 
 while True:
 
@@ -24,14 +80,14 @@ while True:
 	if frame is None:
 		break
 
-	frame = imutils.resize(frame, width=600)
+	frame = imutils.resize(frame, width=640, height=480)
 	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
 	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
 	# ----- Cube initialization -----
 
-	purpleLower = (102, 0, 124)
-	purpleUpper = (180, 179, 255)
+	purpleLower = (113, 34, 71)
+	purpleUpper = (168, 255, 255)
 
 	cubeMask = cv2.inRange(hsv, purpleLower, purpleUpper)
 	cubeMask = cv2.erode(cubeMask, None, iterations=3)
@@ -44,8 +100,8 @@ while True:
 
 	# ----- Cone initialization -----
 
-	yellowLower = (0, 151, 73)
-	yellowUpper = (180, 255, 255)
+	yellowLower = (0, 89, 117)
+	yellowUpper = (88, 255, 255)
 
 	coneMask = cv2.inRange(hsv, yellowLower, yellowUpper)
 	# coneMask = cv2.erode(coneMask, None, iterations=3)
@@ -64,27 +120,39 @@ while True:
 		c = max(cubeCnts, key=cv2.contourArea)
 		((x, y), radius) = cv2.minEnclosingCircle(c)
 		M = cv2.moments(c)
-		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+		if M["m00"] != 0:
+			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 		if radius > 10:
 			cv2.putText(frame, "Cube", (int(x), int(y)), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255))
+			cv2.putText(frame, "Cube X: " + str(int(x)), (0, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255))
 			cv2.circle(frame, (int(x), int(y)), int(radius),
 				(255,0,255), 2)
-			cv2.circle(frame, center, 5, (255, 255, 255), -1)
-
-			print(int(x))
+			if M["m00"] != 0:
+				cv2.circle(frame, center, 5, (255, 255, 255), -1)
+				nTable.putNumber("Cube X", x)
+			else:
+				nTable.putNumber("Cube X", 0)
 
 	if len(coneCnts) > 0:
 		c = max(coneCnts, key=cv2.contourArea)
 		((x, y), radius) = cv2.minEnclosingCircle(c)
 		M = cv2.moments(c)
-		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+		if M["m00"] != 0:
+			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 		if radius > 10:
-			cv2.putText(frame, "Cone", (int(x), int(y)), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255))
+			cv2.putText(frame, "Cone", (int(x), int(y)), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 255))
+			cv2.putText(frame, "Cone X: " + str(int(x)), (0, 70), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255))
 			cv2.circle(frame, (int(x), int(y)), int(radius),
 				(255,0,255), 2)
-			cv2.circle(frame, center, 5, (255, 255, 255), -1)
+			if M["m00"] != 0:
+				cv2.circle(frame, center, 5, (255, 255, 255), -1)
+				nTable.putNumber("Cone X", x)
+			else:
+				nTable.putNumber("Cone X", 0)
 
-			print(int(x))
+	else:
+		nTable.putNumber("Cone X", 0)
+		nTable.putNumber("Cube X", 0)
 
 	cv2.imshow("Game Piece Detection", frame)
 	key = cv2.waitKey(1) & 0xFF
